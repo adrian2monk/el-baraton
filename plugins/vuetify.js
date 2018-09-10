a6d6ee0eda34f9065e2ce2c5d77f57de2f6fbd1a
Adrian Hernandez Barrios <adrian2monk@gmail.com>
2018-09-10 02:39:15 -0500

    Init Rappi frontend test "El Baratón" with Vuetify + Nuxt.js template

---
+++ plugins/vuetify.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#e01a4f',
+    accent: '#7ddf64',
+    secondary: '#f15946',
+    info: '#9c27b0',
+    warning: '#e8aa14',
+    error: '#e01a4f',
+    success: '#7ddf64'
+  }
+})
